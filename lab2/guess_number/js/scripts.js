6acf70e9612b0ff89db56e958622514b3551481d
Solian21 <soliancarlos@gmail.com>
2026-02-15 17:30:52 -0800

    Finished lab2 guess number portion

--- lab2/guess_number/js/scripts.js
+++ lab2/guess_number/js/scripts.js
@@ -5,15 +5,31 @@ document.querySelector("#resetBtn").addEventListener("click", initializeGame);
 //Global variables
 let randomNumber;
 let attempts = 0;
+// this is going to be my variable to keep track of the nuber of games played
+let number_of_games = 0;
+let number_of_games_WON = 0;
+let number_of_games_LOST = 0;
+let scoreboard = document.querySelector("#scoreBoard");
 
 initializeGame();
+updateScoreBoard();
+
+function updateScoreBoard() {
+    scoreboard.textContent = "Wins: " + number_of_games_WON + " | Losses: " + number_of_games_LOST;
+}
 
 function initializeGame() {
+    attempts_left = document.querySelector("#attempts");
+    attempts_left.textContent = "Attempts Left: 7";
+
+
     randomNumber = Math.floor(Math.random() * 99) + 1;
     console.log("randomNumber: " + randomNumber);
 
     //hiding the Reset button
     document.querySelector("#resetBtn").style.display = "none";
+    //making sure to add in guess button
+    document.querySelector("#guessBtn").style.display = "inline"
 
     //adding focus to textbox
     document.querySelector("#playerGuess").focus();
@@ -26,6 +42,7 @@ function initializeGame() {
     feedback.textContent = "";
 
     document.querySelector("#guesses").textContent = "";
+
 }
 
 function checkGuess() {
@@ -44,16 +61,24 @@ function checkGuess() {
     if (guess == randomNumber) {
         feedback.textContent = "You guessed it! You WON!";
         feedback.style.color = "darkgreen";
+        number_of_games_WON++;
+        updateScoreBoard();
         gameOver();
     } else {
-        document.querySelector("#guesses").textContent += guess + " ";
+        document.querySelector("#guesses").textContent += guess + " | ";
         if (attempts == 7) {
-            feedback.textContent = "Sorry, you lost!";
+            feedback.textContent = "Sorry, you lost! | Corect Answer: " + randomNumber;
             feedback.style.color = "red";
+            number_of_games_LOST++;
+            updateScoreBoard();
             gameOver();
         } else if (guess > randomNumber) {
             feedback.textContent = "Guess was high";
+            
+            attempts_left.textContent = "Attempts Left: " + (7 - attempts);
         } else {
+            
+            attempts_left.textContent = "Attempts Left: " + (7 - attempts);
             feedback.textContent = "Guess was low";
         }
     }
@@ -62,6 +87,9 @@ function checkGuess() {
 function gameOver() {
     let guessBtn = document.querySelector("#guessBtn");
     let resetBtn = document.querySelector("#resetBtn");
+    attempts_left.display = "none"
     guessBtn.style.display = "none";
     resetBtn.style.display = "inline";
+    attempts = 0;
+    
 }
